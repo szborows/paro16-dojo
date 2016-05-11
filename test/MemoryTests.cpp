63f23b0618a10504f803f2212676b3bdfbf4a478
Sławomir Zborowski <slawomir.zborowski@nokia.com>
2016-05-11 11:37:50 +0200

    moved scenario to the bottom...

--- test/MemoryTests.cpp
+++ test/MemoryTests.cpp
@@ -7,20 +7,11 @@ TEST_CASE( "initially memory cells are set to 0", "[memory]" ) {
     REQUIRE( memory.get() == Memory::MemoryCell{0} );
 }
 
-SCENARIO( "getting after setting gives the same value", "[memory]" ) {
-
-    GIVEN( "memory and a value" ) {
-        Memory memory;
-        constexpr auto value = 42;
-
-        WHEN ("we set the value") {
-            memory.set(value);
-
-            THEN ( "getting value yields same value" ) {
-                REQUIRE( memory.get() == value );
-            }
-        }
-    }
+TEST_CASE( "getting after setting gives the same value", "[memory]" ) {
+    Memory memory;
+    constexpr auto value = 42;
+    memory.set(value);
+    REQUIRE( memory.get() == value );
 }
 
 #if 0
@@ -38,3 +29,19 @@ TEST_CASE( "memory position can be moved around", "[memory]" ) {
 }
 
 #endif
+
+SCENARIO( "getting after setting gives the same value", "[memory-scen]" ) {
+
+    GIVEN( "memory and a value" ) {
+        Memory memory;
+        constexpr auto value = 42;
+
+        WHEN ("we set the value") {
+            memory.set(value);
+
+            THEN ( "getting value yields same value" ) {
+                REQUIRE( memory.get() == value );
+            }
+        }
+    }
+}
